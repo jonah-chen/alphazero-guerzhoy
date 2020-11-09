ea696bdaf4c95a326230dadcbab474d78544d4d7
jonah-chen <jonah.chen@mail.utoronto.ca>
2020-11-08 22:14:31 -0700

    added C function to generate move from policy

--- nptrain.c
+++ nptrain.c
@@ -99,10 +99,48 @@ is_win(PyObject* self, PyObject* args) {
         return Py_BuildValue("i", 3);
     return Py_BuildValue("i", 0);
 }
+
+static PyObject* 
+move_from_policy(PyObject* self, PyObject* args) {
+    PyObject *list3_obj;
+    double thresh;
+    if (!PyArg_ParseTuple(args, "Od", &list3_obj, &thresh))
+        return NULL;
+
+    double **policy;
+
+    //Create C arrays from numpy objects:
+    int typenum = NPY_DOUBLE;
+    PyArray_Descr *descr;
+    descr = PyArray_DescrFromType(typenum);
+    npy_intp dims[2];
+    if (PyArray_AsCArray(&list3_obj, (void **)&policy, dims, 2, descr) < 0) 
+    {
+        PyErr_SetString(PyExc_TypeError, "error converting to c array");
+        return NULL;
+    }
+    int y = SIZE;
+    double cum_tot = 0.0;
+    while (y--)
+    {
+        int x = SIZE;
+        while (x--)
+        {
+            cum_tot += policy[y][x];
+            if (cum_tot > thresh)
+                return Py_BuildValue("ii", y, x);
+        }
+    }
+    return Py_BuildValue("ii", 0, 0);
+}
+
 static PyMethodDef myMethods[] = {
     {"is_win", is_win, METH_VARARGS, "Input numpy array with shape=(SIZE,SIZE,2,) and return the index corresponding to the game state in this array [\"Continue Playing\", \"Black won\", \"White won\", \"Draw\"]"},
+    {"move_from_policy", move_from_policy, METH_VARARGS, "Input numpy array shape=(SIZE,SIZE,) of the policy: The probabilities of making the move at (y,x) that MUST sum up to 1.0 and a random value between zero and one. Return a random (y,x) with that probability distribution."},
     {NULL, NULL, 0, NULL}
 };
+
+
 
 static struct PyModuleDef myModule = {
     PyModuleDef_HEAD_INIT, 
