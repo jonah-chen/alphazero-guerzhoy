a9b0991bcb15f275476383f1aa0e93f9593820bc
jonah-chen <jonah.chen@mail.utoronto.ca>
2020-11-09 18:10:02 -0700

    fixed bug with draw and added 1D policy decision maker

--- nptrain.c
+++ nptrain.c
@@ -29,7 +29,7 @@ is_win(PyObject* self, PyObject* args) {
         int x = SIZE;
         while (x--)
         {
-            if (draw && (board[y][x][0] || board[y][x][1]))
+            if (draw && !board[y][x][0] && !board[y][x][1])
                 draw = 0;
             // horizontal case:
             if (x + 4 < SIZE)
@@ -101,7 +101,7 @@ is_win(PyObject* self, PyObject* args) {
 }
 
 static PyObject* 
-move_from_policy(PyObject* self, PyObject* args) {
+move_from_policy2D(PyObject* self, PyObject* args) {
     PyObject *list3_obj;
     double thresh;
     if (!PyArg_ParseTuple(args, "Od", &list3_obj, &thresh))
@@ -134,9 +134,42 @@ move_from_policy(PyObject* self, PyObject* args) {
     return Py_BuildValue("ii", 0, 0);
 }
 
+static PyObject *
+move_from_policy1D(PyObject *self, PyObject *args)
+{
+    PyObject *list3_obj;
+    double thresh;
+    if (!PyArg_ParseTuple(args, "Od", &list3_obj, &thresh))
+        return NULL;
+
+    double *policy;
+
+    //Create C arrays from numpy objects:
+    int typenum = NPY_DOUBLE;
+    PyArray_Descr *descr;
+    descr = PyArray_DescrFromType(typenum);
+    npy_intp dims[1];
+    if (PyArray_AsCArray(&list3_obj, (void *)&policy, dims, 1, descr) < 0) 
+    {
+        PyErr_SetString(PyExc_TypeError, "error converting to c array");
+        return NULL;
+    }
+    int i = SIZE * SIZE;
+    double cum_tot = 0.0;
+    while (i--)
+    {
+        cum_tot += policy[i];
+        if (cum_tot > thresh)
+            return Py_BuildValue("ii", i/SIZE, i%SIZE);
+    }
+    return Py_BuildValue("ii", 0, 0);
+}
+
+
 static PyMethodDef myMethods[] = {
     {"is_win", is_win, METH_VARARGS, "Input numpy array with shape=(SIZE,SIZE,2,) and return the index corresponding to the game state in this array [\"Continue Playing\", \"Black won\", \"White won\", \"Draw\"]"},
-    {"move_from_policy", move_from_policy, METH_VARARGS, "Input numpy array shape=(SIZE,SIZE,) of the policy: The probabilities of making the move at (y,x) that MUST sum up to 1.0 and a random value between zero and one. Return a random (y,x) with that probability distribution."},
+    {"move_from_policy2D", move_from_policy2D, METH_VARARGS, "Input numpy array shape=(SIZE,SIZE,) of the policy: The probabilities of making the move at (y,x) that MUST sum up to 1.0 and a random value between zero and one. Return a random (y,x) with that probability distribution."},
+    {"move_from_policy1D", move_from_policy1D, METH_VARARGS, "Input numpy array shape=(SIZE*SIZE,) of the policy: The probabilities of making the move at (y,x) that MUST sum up to 1.0 and a random value between zero and one. Return a random (y=i/SIZE,x=i%SIZE) with that probability distribution."},
     {NULL, NULL, 0, NULL}
 };
 
