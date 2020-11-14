5fda77260a48a05482541b9df551606ab162eaf2
jonah-chen <jonah.chen@mail.utoronto.ca>
2020-11-14 01:28:03 -0700

    Depricated for jupyter notebook main runtime

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from copy import copy
 from tensorflow.keras.models import load_model
 
 
-model = load_model('models/2')
-best_model_num = 2
-best_model = copy(model)
+model = load_model('models/4')
+best_model_num = 4
+best_model = load_model('models/4')
 
-for num in range(3, 25):
+for num in range(5, 25):
     generate_data(num, best_model) 
-    train_model(model, num=num, log_name=num)
+    train_model(model, num=num, log_name=num, epochs=20)
     model.save(f'models/{num}')
     
     score, record, black_games, white_games = eval_model(model, best_model)
@@ -20,6 +20,8 @@ for num in range(3, 25):
     # Saves the games. Black first, white second.
     np.save(f'games/{num}v{best_model_num}', black_games)
     np.save(f'games/{best_model_num}v{num}', white_games)
+
+    del black_games, white_games
 
     print(score)
     if score >= 55:
